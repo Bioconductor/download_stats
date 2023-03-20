988e4e6bc5b2819598dff648572206df883fe103
Hervé Pagès <hpages.on.github@gmail.com>
2023-03-20 16:26:56 -0700

    Notify maintainer@bioconductor.org if one of the Python scripts returns an error

--- makeDownloadStatsHTML-for-data-annotation.sh
+++ makeDownloadStatsHTML-for-data-annotation.sh
@@ -1,14 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 #
-
-set -e  # Exit immediately if a simple command exits with a non-zero status
 
 STATS_HOME=/home/biocadmin/STATS
 HTML_STATS_HOME=/home/biocadmin/public_html/stats
 
+notify()
+{
+	addr="$1"
+	stats_script="$0"
+	stats_logdir="~biocadmin/cron.log/stats/"
+	stats_host="stats.bioconductor.org"
+	subject="Download stats problem: $stats_script returned an error!"
+	msg1="Check the logs in $stats_logdir on $stats_host for the details."
+	msg2="Please do NOT reply."
+	# Make sure to set SMTP settings in ~/.mutt/muttrc
+	echo -e "$msg1\n\n$msg2" | mutt -s "$subject" "$addr"
+}
+
 cd $STATS_HOME
 cp main.css $HTML_STATS_HOME
 ./makeDownloadStatsHTML-for-data-annotation.py
+if [ $? -ne 0 ]; then
+	notify maintainer@bioconductor.org
+fi
 
 rsync --delete -ave ssh $HTML_STATS_HOME/ webadmin@master.bioconductor.org:/extra/www/bioc/packages/stats/
 
