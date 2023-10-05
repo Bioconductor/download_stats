2d16e9b73f2b8fc03fa76625673251cc528ab9ff
n1khilmane <nikhilmane1919@gmail.com>
2023-10-05 11:23:22 -0400

    render_page WIP

--- web/app/dbquery.py
+++ web/app/dbquery.py
@@ -1,5 +1,5 @@
 
-import app.dbquery_structures as dbs
+import dbquery_structures as dbs
 
 
 def get_db_connection():
@@ -32,6 +32,12 @@ def dbquery(request: dbs.DbQueryRequest) -> dbs.DbQueryResponse:
         case dbs.QueryRequestType.PACKAGE_COUNTS:
             # TODO Mean of trailing 12 month ip counts
             u = dbs.DbResultEntry(request.package_name, '2021-03-01', False, 1422, None)
+            r = dbs.DbQueryResponse(status=dbs.DataRetrievalStatus.SUCCESS,
+                                    result=[u])
+            return r
+        case dbs.QueryRequestType.PACKAGE_NAMES:
+            # TODO THIS IS A STUB
+            u = dbs.DbResultEntry(['Bob'], '2021-03-01', False, 1422, None)
             r = dbs.DbQueryResponse(status=dbs.DataRetrievalStatus.SUCCESS,
                                     result=[u])
             return r
